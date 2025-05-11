4a9c2f0bffc78ba43380605b35609ad4affbab47
lexusegorov <alexey.egorov.job@gmail.com>
2025-05-12 01:13:20 +0700

    feat: add logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,3 +1,15 @@
 package logger
 
-//TODO: init logger
+import "github.com/sirupsen/logrus"
+
+func New() *logrus.Logger {
+	logger := logrus.New()
+
+	logger.SetLevel(logrus.DebugLevel)
+	logger.SetFormatter(&logrus.TextFormatter{
+		ForceColors:   true,
+		FullTimestamp: true,
+	})
+
+	return logger
+}
