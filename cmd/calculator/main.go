d6242f833dff721b72274e21f0a6bc45833bf95c
lexusegorov <alexey.egorov.job@gmail.com>
2025-05-12 18:14:21 +0700

    feat: add init + start app

--- cmd/calculator/main.go
+++ cmd/calculator/main.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"api-calculator/internal/app"
+	"api-calculator/internal/logger"
+)
+
 func main() {
-	//TODO: init app
-	//TODO: start app
-	//TODO: graceful shutdown
+	log := logger.New()
+	app := app.New(log)
+
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,31 @@
 package app
 
-//TODO: new app
-//TODO: init server, calc, storage
-//TODO: start app
-//TODO: stop app (graceful)
+import (
+	"github.com/sirupsen/logrus"
+
+	"api-calculator/internal/calculator"
+	"api-calculator/internal/server"
+)
+
+type App struct {
+	s *server.Server
+	l *logrus.Logger
+}
+
+func New(logger *logrus.Logger) *App {
+	//TODO: cache
+	//TODO: storage
+
+	controller := calculator.New(logger, nil, nil)
+	s := server.New(controller)
+
+	return &App{
+		s: s,
+		l: logger,
+	}
+}
+
+func (a App) Run() error {
+	a.l.Info("Starting app...")
+	return a.s.Run()
+}
