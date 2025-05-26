9a336038bf728dcec5ccafd18a21b7d7c9609c78
lexusegorov <alexey.egorov.job@gmail.com>
2025-05-26 17:25:29 +0700

    feat: add tests for parser

--- internal/clevercalc/parser_test.go
+++ internal/clevercalc/parser_test.go
@@ -3,6 +3,8 @@ package clevercalc
 import (
 	"reflect"
 	"testing"
+
+	"github.com/LexusEgorov/api-calculator/internal/models"
 )
 
 var parserTest = newParser()
@@ -65,6 +67,46 @@ func TestRemoveSpaces(t *testing.T) {
 	res := removeSpaces(input)
 
 	if res != expected {
+		t.Errorf("Not equal!\nGot: %v\nExpected: %v\n", res, expected)
+	}
+}
+
+func TestAddNum(t *testing.T) {
+	input := "10"
+	destination := make([]string, 0)
+
+	addNum(&destination, &input)
+
+	if len(destination) == 0 {
+		t.Errorf("destination slice is empty")
+	}
+
+	if destination[0] != "10" {
+		t.Errorf("Not equal!\nGot: %v\nExpected: %v\n", destination[0], "10")
+	}
+
+	if input != "" {
+		t.Errorf("input num isn't empty: got '%v'", input)
+	}
+}
+
+func TestGetActions(t *testing.T) {
+	mockStack := Stack{}
+
+	mockStack.Push(models.OPERATION_SUM)
+	mockStack.Push(models.OPERATION_POW)
+	mockStack.Push(models.OPERATION_MULT)
+	mockStack.Push(models.OPERATION_DIV)
+
+	expected := []string{
+		models.OPERATION_DIV,
+		models.OPERATION_MULT,
+		models.OPERATION_POW,
+	}
+
+	res := parserTest.getActions(&mockStack, 2)
+
+	if !reflect.DeepEqual(res, expected) {
 		t.Errorf("Not equal!\nGot: %v\nExpected: %v\n", res, expected)
 	}
 }
